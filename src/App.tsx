d0df433c25968020663c7e1e95b2fcaf2502e198
Gary Tate <me@garytate.co.uk>
2021-08-20 20:15:10 +0100

    Formatted codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,50 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-// Roboto font
-import '@fontsource/roboto';
-import { Container, createTheme, MuiThemeProvider } from '@material-ui/core';
+import "@fontsource/roboto";
+import { Container, createTheme, MuiThemeProvider } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
-import Browser from './components/Browser';
-import Pokedata from './components/Overview';
-import Header from './components/Header';
-import Compare from './components/Compare';
+import Browser from "./components/Browser";
+import Pokedata from "./components/Overview";
+import Header from "./components/Header";
+import Compare from "./components/Compare";
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FAFAFF'
-    },
-    secondary: {
-      main: '#283D58'
-    }
-  }
+	palette: {
+		primary: {
+			main: "#FAFAFF",
+		},
+		secondary: {
+			main: "#283D58",
+		},
+	},
 });
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Router>
-        <div className="App">
-          <Header />
+	return (
+		<MuiThemeProvider theme={theme}>
+			<Router>
+				<div className="App">
+					<Header />
 
-          <Container maxWidth="lg">
-            <Switch>
-              <Route exact path="/">
-                <Browser />
-              </Route>
-              <Route path="/pokemon/:index/compare/:compare">
-                <Compare />
-              </Route>
-              <Route path="/pokemon/:index">
-                <Pokedata />
-              </Route>
-            </Switch>
-          </Container>
-        </div>
-      </Router>
-    </MuiThemeProvider>
-  );
+					<Container maxWidth="lg">
+						<Switch>
+							<Route exact path="/">
+								<Browser />
+							</Route>
+							<Route path="/pokemon/:index/compare/:compare">
+								<Compare />
+							</Route>
+							<Route path="/pokemon/:index">
+								<Pokedata />
+							</Route>
+						</Switch>
+					</Container>
+				</div>
+			</Router>
+		</MuiThemeProvider>
+	);
 }
 
 export default App;
--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,18 @@
-
 // We know for certain the first index of each
 // generation, making a simple look-up table
-export const GenerationFirstPokemon = [
-	0,
-	151,
-	251,
-	386,
-	493,
-	649,
-	721,
-	809,
-]
+export const GenerationFirstPokemon = [0, 151, 251, 386, 493, 649, 721, 809];
 
 export interface PokecardData {
-	name: "loading...",
-	url: ""
+	name: "loading...";
+	url: "";
 }
 
 export interface PokemonOverview {
-	name: "Generic Pokemon",
-	index: 0,
-	types: {},
-	abilities: {},
-	stats: {},
-	icon: "",
-	category: ""
+	name: "Generic Pokemon";
+	index: 0;
+	types: {};
+	abilities: {};
+	stats: {};
+	icon: "";
+	category: "";
 }
